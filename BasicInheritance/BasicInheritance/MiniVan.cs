09affa414ca27670f709443abeabe1ee31e58960
maksymenkool <maksymenkool@gmail.com>
2017-04-05 12:12:39 +0300

    inheritance and sealed

---
+++ BasicInheritance/BasicInheritance/MiniVan.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicInheritance
+{
+    //MiniVan "is" Car
+    //class MiniVan : Car
+    //Класс MiniVan не может быть расширен! sealed - запечатанный.
+    //Все структуры неявно запечатанные.
+    sealed class MiniVan : Car
+    {
+
+    }
+}
---
+++ BasicInheritance/BasicInheritance/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Создать экземпляр класса Car и установить макс.скорость в 80.
+            Car myCar = new Car(80);
+
+            //Установить текущую скорость.
+            myCar.Speed = 50;
+            Console.WriteLine("My Car is going {0} MPH", myCar.Speed);
+            Console.WriteLine("*********************************");
+            MiniVan myVan = new MiniVan();
+            myVan.Speed = 10;
+            Console.WriteLine("My Car is going {0} MPH", myVan.Speed);
+
+            Console.ReadLine();
+        }
+    }
+}
