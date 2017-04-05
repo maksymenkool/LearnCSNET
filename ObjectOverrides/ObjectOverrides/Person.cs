3893c8183898f3524efe60cad5cfe3d46af09b44
maksymenkool <maksymenkool@gmail.com>
2017-04-05 18:19:59 +0300

    ToString, Equals, GetHashCode

---
+++ ObjectOverrides/ObjectOverrides/Person.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOverrides
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public Person(string fName, string lName, int personAge)
+        {
+            FirstName = fName;
+            LastName = lName;
+            Age = personAge;
+        }
+
+        public Person() {}
+
+        public override string ToString()
+        {
+            //return base.ToString();
+            string myState;
+            myState = string.Format("[First Name: {0}; Last Name: {1}; Age: {2}]",
+                FirstName, LastName, Age);
+            return myState;
+        }
+
+        /*public override bool Equals(object obj)
+        {
+            //return base.Equals(obj);
+            Person temp;
+            temp = (Person)obj;
+            if (temp.FirstName == this.FirstName
+                && temp.LastName == this.LastName
+                && temp.Age == this.Age)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }*/
+
+        public override bool Equals(object obj)
+        {
+            //Когда есть правильно переопред. метод
+            //ToString() можно использовать его.
+            return obj.ToString() == this.ToString();
+        }
+
+        //Возвратить хеш-код на основе значения ToString() объекта Person.
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+    }
+}
---
+++ ObjectOverrides/ObjectOverrides/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOverrides
+{
+    class Program
+    {
+        static void StaticMembersOfObj()
+        {
+            Person p3 = new Person("Sally", "Jones", 4);
+            Person p4 = new Person("Sally", "Jones", 4);
+            Console.WriteLine("p3 and p4 have same state: {0}", object.Equals(p3, p4));
+
+            Console.WriteLine("p3 and p4 are pointing to same object: {0}",
+                object.ReferenceEquals(p3, p4));
+        }
+
+        static void Main(string[] args)
+        {
+            Person p1 = new Person("Homer", "Simpson", 50);
+            Person p2 = new Person("Homer", "Simpson", 50);
+
+            Console.WriteLine("p1.ToString() = {0}", p1.ToString());
+            Console.WriteLine("p2.ToString() = {0}", p2.ToString());
+
+            Console.WriteLine("p1 = p2?: {0}", p1.Equals(p2));
+
+            Console.WriteLine("Same hash codes?: {0}", p1.GetHashCode() == p2.GetHashCode());
+            Console.WriteLine();
+
+            p2.Age = 45;
+            Console.WriteLine("p1.ToString() = {0}", p1.ToString());
+            Console.WriteLine("p2.ToString() = {0}", p2.ToString());
+            Console.WriteLine("p1 = p2?: {0}", p1.Equals(p2));
+            Console.WriteLine("Same hash codes?: {0}", p1.GetHashCode() == p2.GetHashCode());
+            Console.WriteLine();
+
+            StaticMembersOfObj();
+            Console.ReadLine();
+        }
+    }
+}
