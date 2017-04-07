3366280dc3028a5d24de8ab73a9926ed41f0ab2a
maksymenkool <maksymenkool@gmail.com>
2017-04-07 18:21:03 +0300

    IEnumerable, IEnumerator

---
+++ CustomEnumerator/CustomEnumerator/Garage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomEnumerator
+{
+    public class Garage : IEnumerable
+    {
+        //System.Array уже реализует IEnumerator
+        private Car[] carArray = new Car[4];
+
+        //Заполнить первоначально несколькими объектами Car.
+        public Garage()
+        {
+            carArray[0] = new Car("Rusty", 30);
+            carArray[1] = new Car("Clunker", 55);
+            carArray[2] = new Car("Zippy", 30);
+            carArray[3] = new Car("Fred", 30);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            //Возвратить IEnumerator объекта массива.
+            return carArray.GetEnumerator();
+        }
+
+        //Чтобы скрыть функциональность IEnumerator на уровне объекта,
+        //достаточно воспользоваться явной реализацией интерфейса:
+        /*IEnumerator IEnumerable.GetEnumerator()
+        {
+            //Возвратить IEnumerator объекта массива.
+            return carArray.GetEnumerator();
+        }*/
+    }
+}
---
+++ CustomEnumerator/CustomEnumerator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomEnumerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Fun with IEnumerable/IEnumerator*****\n");
+            Garage carLot = new Garage();
+            //Проход по всем объектам Car в коллекции?
+            //В классе Garage не реализован метол GetEnumerator()
+            //определенный в интерфейсе IEnumerable. Реализуем его
+            //и все будет работать ))
+            foreach(Car c in carLot)
+            {
+                Console.WriteLine("{0} is going {1} MPH",
+                    c.PetName, c.CurrentSpeed);
+            }
+            Console.ReadLine();
+        }
+    }
+}
