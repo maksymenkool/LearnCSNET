80cabc1a3b99c3bc2e392ffdbc155423f4abe04e
maksymenkool <maksymenkool@gmail.com>
2017-04-07 22:35:00 +0300

    added Clone method

---
+++ CloneablePoint/CloneablePoint/Point.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloneablePoint
+{
+    class Point : ICloneable
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public PointDescription desc = new PointDescription();
+
+        public Point(int xPos, int yPos, string petName)
+        {
+            X = xPos; Y = yPos;
+            desc.PetName = petName;
+        }
+
+        public Point(int xPos, int yPos)
+        {
+            X = xPos; Y = yPos;
+        }
+        public Point() {}
+
+        //Переопределить Object.ToString().
+        public override string ToString()
+        {
+            return string.Format("X = {0}; Y = {1}; Name = {2};\nID = {3}.\n",
+                X, Y, desc.PetName, desc.PointID);
+        }
+
+        //Возвратить копию текущего объекта.
+        public object Clone()
+        {
+            //Если класс или структура содержит только типы значений.
+            //Копировать каждое поле почленно.
+            //return this.MemberwiseClone();
+
+            //Если содержит специальный тип, поддерживающий ссылочные
+            //типы, для построения глубокой копии потребуется создание нового
+            //объекта, который учитывает каждую перем. ссылочного типа.
+            Point newPoint = (Point)this.MemberwiseClone();
+
+            //Теперь заполним пробелы.
+            PointDescription currentDesc = new PointDescription();
+            currentDesc.PetName = this.desc.PetName;
+            newPoint.desc = currentDesc;
+            return newPoint;
+        }
+    }
+}
---
+++ CloneablePoint/CloneablePoint/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloneablePoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(100, 100, "Jane");
+            Point p2 = (Point)p1.Clone();
+            Console.WriteLine("Before modification:");
+            Console.WriteLine("p1: {0}", p1);
+            Console.WriteLine("p2: {0}", p2);
+            p2.desc.PetName = "My new Point";
+            p2.X = 9;
+            Console.WriteLine("After modification:");
+            Console.WriteLine("p1: {0}", p1);
+            Console.WriteLine("p2: {0}", p2);
+        }
+    }
+}
