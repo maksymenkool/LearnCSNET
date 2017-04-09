4dbd855e4e3f4ce39976bbd08cca6b32ed7b9a14
maksymenkool <maksymenkool@gmail.com>
2017-04-09 17:52:03 +0300

    delegate with events

---
+++ CarEvents/CarEvents/Car.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarEvents
+{
+    class Car
+    {
+        //Данные состояния.
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; }
+        public string PetName { get; set; }
+
+        //Исправлен ли автомобиль?
+        private bool carIsDead;
+
+        //Конструкторы класса.
+        public Car() { MaxSpeed = 100; }
+
+        public Car(string name, int maxSp, int currSp)
+        {
+            CurrentSpeed = currSp;
+            MaxSpeed = maxSp;
+            PetName = name;
+        }
+
+        //1. Определить тип делегата.
+        public delegate void CarEngineHandler(string msgForCaller);
+        //Car может посылать следующие события.
+        public event CarEngineHandler Exploded;
+        public event CarEngineHandler AboutToBlow;
+
+        //2. Определить переменную-член этого делегата.
+        private CarEngineHandler listOfHandlers;
+        //3. Добавить регистрационную функцию для вызывающего кода.
+        public void RegisterWithCarEngine(CarEngineHandler methodToCall)
+        {
+            //listOfHandlers = methodToCall;
+
+            //Добавление поддержки группового вызова.
+            //использование операции +=
+            //listOfHandlers += methodToCall;
+
+            //Вместо += исп. Delegate.Combine()
+            if (listOfHandlers == null)
+                listOfHandlers = methodToCall;
+            else
+                Delegate.Combine(listOfHandlers, methodToCall);
+        }
+
+        public void UnRegisterWithCarEngine(CarEngineHandler methodToCall)
+        {
+            listOfHandlers -= methodToCall;
+        }
+
+        //4. Реализовать метод Accelerate() для обращения
+        //   к списку вызовов делегата при нужных условиях.
+        public void Accelerate(int delta)
+        {
+            //Если этот автомобиль сломан, инициировать событие Exploded
+            if (carIsDead)
+            {
+                if (Exploded != null)
+                    Exploded("Sorry, this car is dead...");
+            }
+            else
+            {
+                CurrentSpeed += delta;
+                //Автомобиль почти сломан?
+                if (10 == (MaxSpeed - CurrentSpeed) 
+                    && AboutToBlow != null)
+                {
+                    AboutToBlow("Careful buddy! Gonna blow!");
+                }
+                //Все в порядке!
+                if (CurrentSpeed >= MaxSpeed)
+                    carIsDead = true;
+                else
+                    Console.WriteLine("CurrentSpeed = {0}", CurrentSpeed);
+            }
+        }
+    }
+}
---
+++ CarEvents/CarEvents/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c1 = new CarEvents.Car("SlugBug", 100, 10);
+
+            //Зарегистрировать обработчик событий.
+            c1.AboutToBlow += new Car.CarEngineHandler(CarIsAlmostDoomed);
+            //или так  c1.AboutToBlow += CarIsAlmostDoomed;
+            c1.AboutToBlow += new Car.CarEngineHandler(CarAboutToBlow);
+            //или так  c1.AboutToBlow += CarAboutToBlow;
+
+            Car.CarEngineHandler d = new Car.CarEngineHandler(CarExploded);
+            c1.Exploded += d;
+            //или так  c1.Exploded += CarExploded;
+
+            Console.WriteLine("***** Speeding up *****");
+            for (int i = 0; i < 6; i++)
+                c1.Accelerate(20);
+
+            //Удалить метод CarExploded из списка вызовов.
+            c1.Exploded -= d;
+            //или так  c1.Exploded -= CarExploded;
+
+            Console.WriteLine("***** Speeding up *****");
+            for (int i = 0; i < 6; i++)
+                c1.Accelerate(20);
+            Console.ReadLine();
+        }
+
+        public static void CarAboutToBlow(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        public static void CarIsAlmostDoomed(string msg)
+        {
+            Console.WriteLine("=> Critical Message from Car: {0}", msg);
+        }
+
+        public static void CarExploded(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
